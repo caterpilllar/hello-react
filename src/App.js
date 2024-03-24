aad12179524acb709f83c757bf5776a3235c43aa
caterpilllar <123208987+caterpilllar@users.noreply.github.com>
2024-03-23 22:08:46 -0400

    react changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MI 449 Lab 9
         </a>
       </header>
     </div>
